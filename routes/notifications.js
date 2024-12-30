32879a5df7fed1ed1a09f57761b40ccab286ade8
Akinwalee <obatulafuad@gmail.com>
2024-12-30 12:43:36 +0100

    Update the backend project structure

--- routes/notifications.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { sendNotification, sendBulkNotifications } from '../controllers/notificationController.js';
-
-const router = express.Router();
-
-router.post('/send', sendNotification); // Send notification to a single user
-router.post('/send-bulk', sendBulkNotifications); // Send bulk notifications
-
-export default router;
--- services/incomeService.js
+++
@@ -1,7 +0,0 @@
-import { IncomeModel } from '../models/incomeModel.js';
-
-export const IncomeService = {
-  addIncome: async (userId, amount, source) => {
-    return await IncomeModel.create(userId, amount, source);
-  },
-};
