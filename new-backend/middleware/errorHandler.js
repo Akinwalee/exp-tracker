d7257f4546e67e4b42fdbdaee06fe47a56f868d1
Akinwalee <obatulafuad@gmail.com>
2024-12-30 13:13:26 +0100

    Cleaning things up

--- new-backend/middleware/errorHandler.js
+++
@@ -1,9 +0,0 @@
-export const errorHandler = (err, req, res, next) => {
-  console.error(err.stack);
-
-  if (process.env.NODE_ENV === 'production') {
-    res.status(500).json({ error: 'Something went wrong, please try again later.' });
-  } else {
-    res.status(500).json({ error: err.message, stack: err.stack });
-  }
-};
