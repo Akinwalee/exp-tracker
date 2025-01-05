52f9788b96eb2b8488e2d2a3af84a26faa56107b
Akinwalee <obatulafuad@gmail.com>
2025-01-05 06:31:37 +0100

    Integrate Sidebar component into dasboard and implement the dynamic display logic

--- exp-tracker-frontend/src/pages/Dashboard.jsx
+++ exp-tracker-frontend/src/pages/Dashboard.jsx
@@ -1,33 +1,25 @@
-// import { useEffect } from 'react';
-// import { useAuth } from '../contexts/AuthContext';
-// import { useNavigate } from 'react-router-dom';
-import Navbar from '../components/NavBar';
+import { useState } from 'react';
+import Sidebar from '../components/Sidebar';
 import Overview from '../components/Overview';
-import Transactions from '../components/Transactions';
 import Savings from '../components/Savings';
 import BudgetManagement from '../components/BudgetManagement';
 import ProtectedRoute from '../components/ProtectedRoute';
+import AddIncome from '../components/AddIncome';
+import IncomeList from '../components/IncomeList';
 
 const Dashboard = () => {
-  // const { user } = useAuth(); // Access the user state from AuthContext
-  // const navigate = useNavigate();
-
-  // useEffect(() => {
-  //   if (!user) {
-  //     navigate('/signin'); // Redirect non-logged-in users to sign-in page
-  //   }
-  // }, [user, navigate]);
+  const [activeTab, setActiveTab] = useState('overview'); // Default active tab
 
   return (
-    <ProtectedRoute> 
-      <div className="bg-background min-h-screen">
-        <Navbar />
-        <div className="max-w-7xl mx-auto py-8 px-4 sm:px-6 lg:px-8">
-          <h1 className="text-3xl font-bold text-gray-900 mb-5">Dashboard</h1>
-          <Overview />
-          <Transactions />
-          <Savings />
-          <BudgetManagement />
+    <ProtectedRoute>
+      <div className="flex bg-background min-h-screen">
+        <Sidebar activeTab={activeTab} setActiveTab={setActiveTab} />
+        <div className="w-3/4 p-4">
+          {/* <h1 className="text-3xl font-bold text-gray-900 mb-5">Dashboard</h1> */}
+          {activeTab === 'overview' && <Overview />}
+          {activeTab === 'income' && <><IncomeList /><AddIncome /></>}
+          {activeTab === 'savings' && <Savings />}
+          {activeTab === 'budgets' && <BudgetManagement />}
         </div>
       </div>
     </ProtectedRoute>
